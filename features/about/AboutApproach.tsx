bc245d5573e80ac9f536aac2d4199606970a9e2d
Jonathan Olbes <olbes.es@gmail.com>
2026-06-09 23:23:11 +0200

    fix: Font Size about page

--- features/about/AboutApproach.tsx
+++ features/about/AboutApproach.tsx
@@ -2,11 +2,11 @@ export function AboutApproach() {
   return (
     <section className="py-28">
       <div className="mx-auto max-w-3xl px-6 text-center">
-        <h2 className="text-3xl font-semibold text-text mb-6">
+        <h2 className="text-3xl md:text-5xl font-semibold text-text mb-6">
           Una forma diferente de trabajar
         </h2>
 
-        <p className="text-muted leading-relaxed">
+        <p className="md:text-md text-muted leading-relaxed">
           No creemos en proyectos rápidos sin estrategia. Nos enfocamos en
           entender el negocio, detectar oportunidades y construir soluciones que
           realmente aporten valor.
--- features/about/AboutCredibility.tsx
+++ features/about/AboutCredibility.tsx
@@ -4,18 +4,24 @@ export function AboutCredibility() {
       <div className="mx-auto max-w-6xl px-6">
         <div className="grid gap-10 md:grid-cols-3 text-center">
           <div>
-            <p className="text-2xl font-semibold text-primary">+30</p>
-            <p className="text-sm text-muted">Proyectos trabajados</p>
+            <p className="py-2 text-4xl md:py-10 md:text-6xl font-semibold text-primary">
+              +30
+            </p>
+            <p className="text-md text-muted">Proyectos trabajados</p>
           </div>
 
           <div>
-            <p className="text-2xl font-semibold text-primary">+5</p>
-            <p className="text-sm text-muted">Sectores diferentes</p>
+            <p className="py-2 text-4xl md:py-10 md:text-6xl font-semibold text-primary">
+              +5
+            </p>
+            <p className="text-md text-muted">Sectores diferentes</p>
           </div>
 
           <div>
-            <p className="text-2xl font-semibold text-primary">100%</p>
-            <p className="text-sm text-muted">Enfoque en resultados</p>
+            <p className="py-2 text-4xl md:py-10 md:text-6xl font-semibold text-primary">
+              100%
+            </p>
+            <p className="text-md text-muted">Enfoque en resultados</p>
           </div>
         </div>
       </div>
