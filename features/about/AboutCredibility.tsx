cd26d962b81b190410b582c130312f6043244371
Jonathan Olbes <olbes.es@gmail.com>
2026-06-12 19:05:59 +0200

    fix: About Credibility section duplicated

--- features/about/AboutCredibility.tsx
+++ features/about/AboutCredibility.tsx
@@ -26,24 +26,6 @@ export function AboutCredibility() {
             <p className="text-sm md:text-lg text-muted">
               Enfoque en resultados
             </p>
-            <p className="py-2 text-4xl md:py-10 md:text-6xl font-semibold text-primary">
-              +30
-            </p>
-            <p className="text-md text-muted">Proyectos trabajados</p>
-          </div>
-
-          <div>
-            <p className="py-2 text-4xl md:py-10 md:text-6xl font-semibold text-primary">
-              +5
-            </p>
-            <p className="text-md text-muted">Sectores diferentes</p>
-          </div>
-
-          <div>
-            <p className="py-2 text-4xl md:py-10 md:text-6xl font-semibold text-primary">
-              100%
-            </p>
-            <p className="text-md text-muted">Enfoque en resultados</p>
           </div>
         </div>
       </div>
